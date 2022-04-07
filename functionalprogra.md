1c099881d5dc52e7f8daf54127cc6e446ada6ff4
niiwade <niilantewade77@gmail.com>
2022-04-07 20:41:05 +0000

    added

---
+++ functionalprogra.md
@@ -0,0 +1,8 @@
+
+In functional programming , functions are first class citizens. 
+We can assign them to a variable, pass them as an argument and return them as another functions.
+
+
+Higher-order functions
+
+It takes a function as an argument and returns
---
+++ src/functionalprogram.js
@@ -0,0 +1,62 @@
+//assign as a variable
+
+function sayHello() {
+    return "Hello World"
+}
+
+let fn = sayHello()
+
+fn()
+
+
+//Pass an argument
+
+function greet(fnMessage) {
+    console.log(fnMessage())
+}
+
+greet(sayHello)
+
+
+//Return a function
+
+function sayHello() {
+    return function () {
+        return "Hello World"
+    }
+}
+
+let message = sayHello()
+
+let text = message()
+
+
+//higher order function
+
+let numbers = [1, 23, 32, 35, 23]
+
+numbers.map(number => number * 2)
+
+
+setTimeout(() => console.log("hellooo"), 1000)
+
+import { compose, pipe } from "lodash/fp"
+
+let input = " Javascript is awesome "
+let output = "<div>" + input.trim() + "</div>"
+
+
+
+const trim = str => str.trim()
+
+const wrapDiv = str => `<div>${str}</div>`
+
+const wrapSpan = str => `<span>${str}</span>`
+
+const toLowerCase = str => str.toLowerCase()
+
+const transform = pipe(trim, toLowerCase, wrapDiv)
+transform(input)
+
+
+
